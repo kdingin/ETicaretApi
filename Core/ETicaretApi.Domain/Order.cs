49242b24e73394a0aa8b2c013fcbfbe0bf5bfdbb
fatih Küçükdingin <68949521+kdingin@users.noreply.github.com>
2024-05-23 23:42:14 +0300

    23.05

---
+++ Core/ETicaretApi.Domain/Order.cs
@@ -0,0 +1,18 @@
+﻿using ETicaretApi.Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretApi.Domain
+{
+    public class Order:BaseEntity
+    {
+        public int customerId { get; set; }
+        public string Description { get; set; }
+        public string Address { get; set; }
+        public ICollection<Order> orders { get; set; }
+        public Customer customer { get; set; }
+    }
+}
