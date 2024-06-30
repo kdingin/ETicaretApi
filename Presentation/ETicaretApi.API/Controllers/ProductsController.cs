42622fa9b8d4b007e4bcbb9a228d9e18bfd07cd4
fatih Küçükdingin <68949521+kdingin@users.noreply.github.com>
2024-06-30 21:17:37 +0300

    file upload

--- Presentation/ETicaretApi.API/Controllers/ProductsController.cs
+++ Presentation/ETicaretApi.API/Controllers/ProductsController.cs
@@ -4,37 +4,48 @@ using ETicaretApi.Application.ViewModels.Products;
 using ETicaretApi.Domain.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System.Net;
-using System.Xml.Linq;
 
 namespace ETicaretApi.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ProductsController : ControllerBase// TEST AMAÇLIDIR.
+    public class ProductsController : ControllerBase
     {
-        readonly private IProductReadRepository _productReadRepository;
-        readonly private IProductWriteRepository _productWriteRepository;
-    
-        public ProductsController(IProductReadRepository productReadRepository, IProductWriteRepository productWriteRepository)
+        private readonly IProductReadRepository _productReadRepository;
+        private readonly IProductWriteRepository _productWriteRepository;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        private readonly ILogger<ProductsController> _logger;
+
+        public ProductsController(
+            IProductReadRepository productReadRepository,
+            IProductWriteRepository productWriteRepository,
+            IWebHostEnvironment webHostEnvironment,
+            ILogger<ProductsController> logger)
         {
             _productReadRepository = productReadRepository;
-            _productWriteRepository = productWriteRepository; 
+            _productWriteRepository = productWriteRepository;
+            _webHostEnvironment = webHostEnvironment;
+            _logger = logger;
         }
 
         [HttpGet]
-        public async Task<IActionResult> Get([FromQuery]Pagination pagination)
+        public async Task<IActionResult> Get([FromQuery] Pagination pagination)
         {
-            var totalCount=_productReadRepository.GetAll(false).Count();
-            var products = _productReadRepository.GetAll(false).Skip(pagination.Page * pagination.Size).Take(pagination.Size).Select(p => new
-            {
-                p.Id,
-                p.Name,
-                p.Price,
-                p.Stock,
-                p.CreatedDate,
-                p.UpdatedDate,
-            }).ToList();
+            var totalCount = _productReadRepository.GetAll(false).Count();
+            var products = _productReadRepository.GetAll(false)
+                .Skip(pagination.Page * pagination.Size)
+                .Take(pagination.Size)
+                .Select(p => new
+                {
+                    p.Id,
+                    p.Name,
+                    p.Price,
+                    p.Stock,
+                    p.CreatedDate,
+                    p.UpdatedDate,
+                }).ToList();
 
             return Ok(new
             {
@@ -44,33 +55,43 @@ namespace ETicaretApi.API.Controllers
         }
 
         [HttpGet("{id}")]
-        public async Task<IActionResult>Get(string id){
-            return Ok(await _productReadRepository.GetByIdAsync(id,false));
+        public async Task<IActionResult> Get(string id)
+        {
+            return Ok(await _productReadRepository.GetByIdAsync(id, false));
         }
 
         [HttpPost]
         public async Task<IActionResult> Post(VM_Create_Product model)
         {
-            if (ModelState.IsValid) { }
-            await _productWriteRepository.AddAsync(new()
+            if (ModelState.IsValid)
             {
-                Name = model.Name,
-                Price = model.Price,
-                Stock = model.Stock,
-            });
-            await _productWriteRepository.SaveAsync();
-            return StatusCode((int)HttpStatusCode.Created);
+                await _productWriteRepository.AddAsync(new Product
+                {
+                    Name = model.Name,
+                    Price = model.Price,
+                    Stock = model.Stock,
+                });
+                await _productWriteRepository.SaveAsync();
+                return StatusCode((int)HttpStatusCode.Created);
+            }
+
+            return BadRequest(ModelState);
         }
 
         [HttpPut]
         public async Task<IActionResult> Put(VM_Update_Product model)
         {
-            Product product= await _productReadRepository.GetByIdAsync(model.Id);
-            product.Stock = model.Stock;
-            product.Price = model.Price;
-            product.Name = model.Name;
-            await _productWriteRepository.SaveAsync();
-            return Ok();
+            Product product = await _productReadRepository.GetByIdAsync(model.Id);
+            if (product != null)
+            {
+                product.Stock = model.Stock;
+                product.Price = model.Price;
+                product.Name = model.Name;
+                await _productWriteRepository.SaveAsync();
+                return Ok();
+            }
+
+            return NotFound();
         }
 
         [HttpDelete("{id}")]
@@ -81,5 +102,25 @@ namespace ETicaretApi.API.Controllers
             return Ok();
         }
 
+        [HttpPost("[action]")]
+        public async Task<IActionResult> Upload()
+        {
+
+            string uploadPath = Path.Combine(_webHostEnvironment.WebRootPath, "resource/product-images");
+            if (!Directory.Exists(uploadPath))
+            {
+                Directory.CreateDirectory(uploadPath);
+            }
+            Random r = new Random();
+            foreach (IFormFile file in Request.Form.Files)
+            {
+                string fullPath = Path.Combine(uploadPath, $"{r.Next()}{Path.GetExtension(file.FileName)}");
+
+                using FileStream fileStream = new(fullPath, FileMode.Create, FileAccess.Write, FileShare.None, 1024 * 1024, useAsync: false);
+                await file.CopyToAsync(fileStream);
+                await fileStream.FlushAsync();
+            }
+            return Ok();
+        }
     }
 }
